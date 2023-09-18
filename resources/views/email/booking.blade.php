b3eff4f25719bf585136df97fc2f4c6bbeee084b
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2023-09-18 11:27:42 +0700

    mengubah huruf unicode di view email booking

--- resources/views/email/booking.blade.php
+++ resources/views/email/booking.blade.php
@@ -21,8 +21,8 @@
             Mohon maaf kamu belum bisa melakukan kunjungan ke Museum Teknoform dikarenakan jadwal yang kamu pilih bersamaan
             dengan kunjungan pengunjung lain. Kamu bisa coba untuk melakukan appointment kunjungan lagi dengan pilihan hari
             sebagai berikut :</p>
-        <p>Hari : Senin – Jumat<br />
-            Pukul : 09.00 – 16.00 WIB</p>
+        <p>Hari : Senin - Jumat<br />
+            Pukul : 09.00 - 16.00 WIB</p>
         <p>Tidak lupa kami juga mengingatkan bagi para pengunjung untuk mematuhi protokol kesehatan yang diterapkan yaitu
             wajib menggunakan masker, mencuci tangan saat memasuki area kunjungan serta menjaga jarak dengan pengunjung yang
             lain saat di dalam area.
